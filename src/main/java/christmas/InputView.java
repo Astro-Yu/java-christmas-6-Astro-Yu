928d5ed22de0f126f79c939e1b4d0446eae409c6
yush <yush2202@gmail.com>
2023-11-10 06:57:00 +0900

    feat : inputView 구현 사용자 입력과 관련된 기능을 inputView에 구현했습니다.

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -1,5 +1,7 @@
 package christmas;
 
+import static christmas.InputValidator.checkInputBlank;
+import static christmas.InputValidator.checkInputEmpty;
 import static christmas.Utils.splitWithComma;
 import static christmas.Utils.stringToInteger;
 
@@ -11,6 +13,7 @@ public class InputView {
     public int readDate() {
         System.out.println(InputMessages.ENTER_VISIT_DATE);
         String input = Console.readLine();
+        validate(input);
 
         return stringToInteger(input);
     }
@@ -18,7 +21,13 @@ public class InputView {
     public List<String> readOrders(){
         System.out.println(InputMessages.ENTER_ORDER_PROMPT);
         String input = Console.readLine();
+        validate(input);
 
         return splitWithComma(input);
+    }
+
+    private void validate(String input){
+        checkInputEmpty(input);
+        checkInputBlank(input);
     }
 }
