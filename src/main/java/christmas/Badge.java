541c6d8b6de9aaeb2a7fd064a7c3bc156156b023
yush <yush2202@gmail.com>
2023-11-10 20:38:37 +0900

    feat : Badge enum 구현 뱃지의 이름과 가격을 정한 enum을 구현했습니다.

---
+++ src/main/java/christmas/Badge.java
@@ -0,0 +1,23 @@
+package christmas;
+
+public enum Badge {
+    STAR_BADGE("별",5_000),
+    TREE_BADGE("트리",10_000),
+    SANTA("산타",20_000);
+
+    private final String name;
+    private final int criticalPrice;
+
+    Badge(String name, int criticalPrice) {
+        this.name = name;
+        this.criticalPrice = criticalPrice;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getCriticalPrice(){
+        return this.criticalPrice;
+    }
+}
--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -14,11 +14,21 @@ public enum Menu {
     TAPAS("타파스",5_500),
     CAESAR_SALAD("시저샐러드",8_000);
 
-    private String name;
-    private int price;
+    private final String name;
+    private final int price;
 
     Menu(String name,int price) {
         this.name = name;
         this.price = price;
+    }
+
+    public String getName(){
+        return this.name;
+
+    }
+
+    public int getPrice(){
+        return this.price;
+
     }
 }
